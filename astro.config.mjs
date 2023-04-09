fc13c430a2506a6aff1da15dc64968b70ff75cae
栗田久海 <kurita_hisami@liginc.co.jp>
2023-04-09 13:24:42 +0900

    feat: github-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hisamikurita.github.io',
+  base: '/fake-image',
   vite: {
     build: {
       rollupOptions: {
